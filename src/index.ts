ebc637f03d124b536818a3abebf7cac342f35d2e
zce <w@zce.me>
2020-07-18 16:19:31 +0800

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ export interface Options {
   host: string
 }
 
-export default (name: string, options?: Options) => {
+export default (name: string, options?: Options): string => {
   if (typeof name !== 'string') {
     throw new TypeError(`Expected a string, got ${typeof name}`)
   }
 
   options = Object.assign({}, options)
 
-  return `${name}@${options.host || 'zce.me'}`
+  return `${name}@${options.host ?? 'zce.me'}`
 }
