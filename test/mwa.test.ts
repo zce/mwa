d7682db657f78527230c785fc042c54f98c42636
Elon Tang <blackcater2015@gmail.com>
2021-04-30 15:55:56 +0800

    fix: Instance of MWA can't run more than once.

--- test/mwa.test.ts
+++ test/mwa.test.ts
@@ -38,7 +38,17 @@ test('middlewares', async () => {
     order.push(8)
   })
 
-  await app.run({ a: 1 })
+  const state1 = { a: 1 }
 
+  await app.run(state1)
+
+  expect(state1).toEqual({ a: 1, b: 2, c: 3, d: 4 })
   expect(order).toEqual([1, 3, 5, 7, 8, 6, 4, 2])
+
+  const state2 = { a: 1, z: -1 }
+
+  await app.run(state2)
+
+  expect(state2).toEqual({ a: 1, b: 2, c: 3, d: 4, z: -1 })
+  expect(order).toEqual([1, 3, 5, 7, 8, 6, 4, 2, 1, 3, 5, 7, 8, 6, 4, 2])
 })
