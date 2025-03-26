886e374f6c9e9a3a3a98bf50729e24ddf23ec30d
zce <w@zce.me>
2025-03-26 20:19:40 +0800

    feat: 2025

--- test/mwa.test.ts
+++ test/mwa.test.ts
@@ -1,4 +1,5 @@
-import { test, expect } from 'vitest'
+import { test } from 'node:test'
+import { deepEqual, strictEqual } from 'node:assert'
 import createMwa from '../src'
 
 test('middlewares', async () => {
@@ -8,7 +9,7 @@ test('middlewares', async () => {
   const app = createMwa<State>()
 
   app.use(async (state, next) => {
-    expect(state.a).toBe(1)
+    strictEqual(state.a, 1)
     state.b = 2
     order.push(1)
     await next()
@@ -17,14 +18,14 @@ test('middlewares', async () => {
 
   app.use([
     async (state, next) => {
-      expect(state.b).toBe(2)
+      strictEqual(state.b, 2)
       state.c = 3
       order.push(3)
       await next()
       order.push(4)
     },
     async (state, next) => {
-      expect(state.c).toBe(3)
+      strictEqual(state.c, 3)
       state.d = 4
       order.push(5)
       await next()
@@ -33,7 +34,7 @@ test('middlewares', async () => {
   ])
 
   app.use(async (state, next) => {
-    expect(state.d).toBe(4)
+    strictEqual(state.d, 4)
     order.push(7)
     await next()
     order.push(8)
@@ -43,13 +44,13 @@ test('middlewares', async () => {
 
   await app.run(state1)
 
-  expect(state1).toEqual({ a: 1, b: 2, c: 3, d: 4 })
-  expect(order).toEqual([1, 3, 5, 7, 8, 6, 4, 2])
+  deepEqual(state1, { a: 1, b: 2, c: 3, d: 4 })
+  deepEqual(order, [1, 3, 5, 7, 8, 6, 4, 2])
 
   const state2 = { a: 1, z: -1 }
 
   await app.run(state2)
 
-  expect(state2).toEqual({ a: 1, b: 2, c: 3, d: 4, z: -1 })
-  expect(order).toEqual([1, 3, 5, 7, 8, 6, 4, 2, 1, 3, 5, 7, 8, 6, 4, 2])
+  deepEqual(state2, { a: 1, b: 2, c: 3, d: 4, z: -1 })
+  deepEqual(order, [1, 3, 5, 7, 8, 6, 4, 2, 1, 3, 5, 7, 8, 6, 4, 2])
 })
